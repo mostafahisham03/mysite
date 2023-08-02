be41387444252e4aaa6887c5e861fa23ecbb97ef
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2023-08-03 00:28:58 +0300

    initialize login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
